564e2e649bd45cb8acde8876a58cf92354b13fa1
김영우 <kyw05171@gmail.com>
2023-01-25 15:48:37 +0900

    DemoLayoutNav 스타일 작성

--- layouts/uiLayouts/DemoLayout/DemoLayoutNav.tsx
+++ layouts/uiLayouts/DemoLayout/DemoLayoutNav.tsx
@@ -1,23 +1,43 @@
+// nextjs
 import Link from 'next/link';
-// import { 
-//     demoSectionMapper,
-//     TDemoSectionMapperKey,
-// } from '@/components/pages/demoPages/_demoSectionMapper';
+import { useRouter } from 'next/router';
+// styled-components
+import styled from 'styled-components';
+import classNames from 'classnames';
+
 import {
     labelrUiDemoRouteMapper,
-    TLabelrUiDemoRouteMapperKey,
 } from '@/router/labelrUiDemoRouteMapper';
-import styled from 'styled-components';
+import { RoutePathFactory } from '@/router/RoutePathFactory';
 
 const StyledDemoLayoutNavRoot = styled.nav`
-    //
+    padding: 12px;
+    width: 100%;
+    height: 100%;
+    
+    display: flex;
+    flex-direction: column;
+
+    box-shadow: -3px 0 9px ${({ theme }) => theme.colors.gs[900]};
 
     .menuItem {
-        //
+        padding: 8px 4px;
+        color: ${({ theme }) => theme.colors.gs[500]};
+        font-weight: 700;
+        transition: all 0.28s ease;
+
+        &.active {
+            color: ${({ theme }) => theme.colors.white};
+            background-color: ${({ theme }) => theme.colors.green[700]};
+        }
     }
 `;
 
 function DemoLayoutNav() {
+    const router = useRouter();
+    const demoName = router.query.demoName as string;
+    const currentPath = RoutePathFactory['/labelrUiDemo/[demoName]'](demoName);
+
     return (
         <StyledDemoLayoutNavRoot>
             {Object.entries(labelrUiDemoRouteMapper).map(([key, routeData]) => {
@@ -27,8 +47,17 @@ function DemoLayoutNav() {
                 } = routeData;
                 
                 return (
-                    <Link key={key} href={path}>
-                        {title}
+                    <Link 
+                        key={key}
+                        href={path}
+                        passHref
+                        legacyBehavior>
+                        <a className={classNames(
+                            'menuItem',
+                            { 'active': path === currentPath}
+                        )}>
+                            {title}
+                        </a>
                     </Link>
                 );
             })}
