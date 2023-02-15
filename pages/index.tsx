c1c0e96951b5c2c28b486259aae0d64f2864b0c5
김영우 <kyw05171@gmail.com>
2023-02-15 18:45:10 +0900

    signinPageReducer 구현 #3

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,9 @@ function HomeIndex() {
 
     // FIXME: 로그인 기능 생성 후, redirect 기능 구현 하기
     useEffect(() => {
-        router.push(RoutePathFactory.account['/signin']());
+        if (router.isReady) {
+            router.push(RoutePathFactory.account['/signin']());
+        }
     }, [router]);
 }
 
